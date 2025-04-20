4c436715006b9f9d428fd964b8496aa9ef3f1c86
Andrey Paes <andreivison1997@gmail.com>
2025-04-20 12:07:30 -0400

    updade css variables

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -5,7 +5,7 @@ const ProductPage = () => {
     <div className="p-5 border border-red-500 rounded-xl> ">
       return <h1 className="text-red-500">products page</h1>
       <Button>Andrey Paes</Button>
-      <input placeholder="bora fechar esse projeto" />
+      <input placeholder="bora fechar esse projeto!" />
     </div>
   );
 };
