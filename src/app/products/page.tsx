b79bf2d88462d1dbc1faba7df94580cd44b144d5
Andrey Paes <andreivison1997@gmail.com>
2025-04-20 11:53:16 -0400

    feat: add shadcn initial setup and components

---
+++ src/app/products/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+
+const ProductPage = () => {
+  return (
+    <div className="p-5 border border-red-500 rounded-xl> ">
+      return <h1 className="text-red-500">products page</h1>
+      <Button>Andrey Paes</Button>
+      <input placeholder="bora fechar esse projeto" />
+    </div>
+  );
+};
+
+export default ProductPage;
